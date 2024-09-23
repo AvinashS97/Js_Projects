eb243b1fe7440418a6bfb77f28bd17caa0da985d
AvinashS97 <avikumar97170@gmail.com>
2024-09-23 22:51:14 +0530

    23Sep

--- 5Note Taking/script.js
+++ 5Note Taking/script.js
@@ -1,33 +1,25 @@
 const addBtn = document.querySelector("#addBtn");
 const main = document.querySelector("#main");
 
-//Add Note by clicking "Add Note"
-addBtn.addEventListener("click", function(){
-    addNote();
-})
-
-//! Self- calling fns
-(
-    function(){
-        const notes = localStorage.getItem("notes");
-    }
-)()
-
-// 22:56
 
 // To save the Notes
-const saveNote = ()=>{
+const saveNotes = () => {
     const notes = document.querySelectorAll(".note textarea");
-    const data =[];
-    notes.forEach((note)=>{
+    console.log(notes);
+    const data = [];
+    notes.forEach((note) => {
         data.push(note.value)
     })
     // console.log(data)
-    localStorage.setItem("notes",JSON.stringify(data));
+    if (data.length == 0) {
+        localStorage.removeItem("notes")
+    } else {
+        localStorage.setItem("notes", JSON.stringify(data));
+    }
 }
 
-// Adding new Notes
-const addNote = ()=>{
+
+const addNote = (text = "") => {
     const note = document.createElement("div");
     note.classList.add("note")
     note.innerHTML = `
@@ -35,19 +27,22 @@ const addNote = ()=>{
     <i class="save fas fa-save"></i>
     <i class="trash fas fa-trash"></i>
     </div> 
-    <textarea></textarea> 
+    <textarea>${text}</textarea> 
     `
-    note.querySelector(".trash").addEventListener("click", function(){
+    note.querySelector(".trash").addEventListener("click", function () {
         note.remove();
     })
-    
-    note.querySelector(".save").addEventListener("click", function(){
-        saveNote();
-    })
 
+    note.querySelector(".save").addEventListener("click", function () {
+        saveNotes();
+    })
+    note.querySelector("textarea").addEventListener("click", function () { saveNotes() })
     main.appendChild(note);
-    saveNote();
+    saveNotes();
 }
+
+// Adding new Notes
+
 
 /*
 <div class="tool">
@@ -56,4 +51,36 @@ const addNote = ()=>{
 </div> 
 <textarea></textarea> 
 */
+
+//! Self- calling fns
+(
+    function () {
+        const lsNotes = JSON.parse(localStorage.getItem("notes"));
+        // console.log(lsNotes);
+        if (lsNotes == null) {
+            addNote();
+        } else {
+            lsNotes.forEach((lsNote) => {
+                addNote(lsNote)
+            })
+        }
+        if (lsNotes.length == 0) {
+            localStorage.removeItem("notes")
+        } else {
+            addNote()
+        }
+    }
+)()
+
+
+//Add Note by clicking "Add Note"
+addBtn.addEventListener("click", function () {
+    addNote();
+})
+
+
+
+// 22:56
+
+
 
