ba856af097e0d031aec214570555169e75c8ed5a
AvinashS97 <avikumar97170@gmail.com>
2024-10-04 21:31:15 +0530

    almost Done Modal

---
+++ 7Modals/script.js
@@ -0,0 +1,19 @@
+// console.log("Hello")
+const modal = document.querySelector(".modal")
+const openModalButton = document.querySelector(".openModalButton")
+const closeButton = document.querySelector(".close_button")
+
+const openModal = () =>{
+    modal.classList.remove("close");
+}
+
+const closeModal = () =>{
+    modal.classList.add("close");
+}
+openModalButton.addEventListener("click", ()=>{
+    openModal();
+})
+
+closeButton.addEventListener("click",()=>{
+    closeModal();
+})
