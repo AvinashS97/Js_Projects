4a692fecaca90b21f75b173591a02bc4e7a9dfad
AvinashS97 <avikumar97170@gmail.com>
2024-11-04 22:17:11 +0530

    All Done 8th Project

--- 8Weather App/script.js
+++ 8Weather App/script.js
@@ -2,13 +2,17 @@ const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
 const form = document.querySelector("form")
 const search = document.querySelector("#search")
 const weather = document.querySelector("#weather")
-    // const API = `https://api.openweathermap.org/data/2.5/weather?
-    // q=${city}&appid=${API_KEY}&units=metric`
+    // const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+
     // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-const getWeather = async(city) => {
+
+const getWeather = async (city) => {
     weather.innerHTML = `<h2> Loading... <h2>`
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    // console.log(url);
     const response = await fetch(url);
+    console.log(response);
+    
     const data = await response.json()
     return showWeather(data)
 }
@@ -32,6 +36,8 @@ const showWeather = (data) => {
 form.addEventListener(
     "submit",
     function(event) {
+        console.log(search.value);
+        
         getWeather(search.value)
         event.preventDefault();
     }
